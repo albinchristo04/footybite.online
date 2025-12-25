8d9eea1c7f479a01943fa6829872c45e6153d22c
albinchristo04 <priyanka0101111@gmail.com>
2025-12-25 17:22:36 +0530

    mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 
 export default defineConfig({
     plugins: [react()],
+    define: {
+        'process.env': {},
+        'process.env.NODE_ENV': '"production"',
+        global: 'window'
+    },
     build: {
         lib: {
-            entry: resolve(__dirname, 'src/client.jsx'),
+            entry: resolve(process.cwd(), 'src/client.jsx'),
             name: 'FootybiteClient',
             fileName: 'client',
             formats: ['iife']
@@ -14,4 +19,4 @@ export default defineConfig({
         outDir: 'dist/assets',
         emptyOutDir: false
     }
-});
+})
